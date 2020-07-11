2d597326598a816e4a83f30432a51bac92db94a5
Tobias França <tobias@aobsoft.com.br>
2020-07-11 14:46:08 -0300

    Ajuste texto do cálculo

--- lib/main.dart
+++ lib/main.dart
@@ -63,8 +63,7 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    FirebaseAdMob.instance
-        .initialize(appId: androidAppId);
+    FirebaseAdMob.instance.initialize(appId: androidAppId);
 
     startBanner();
     displayBanner();
@@ -94,17 +93,23 @@ class _HomeState extends State<Home> {
       double imc = weight / (height * height);
 
       if (imc < 18.6) {
-        _infoText = "Você está abaixo do peso! seu IMC é (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está abaixo do peso! seu IMC é (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso ideal! seu IMC é (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está com o peso ideal! seu IMC é (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Levemente acima do peso! seu IMC é (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está levemente acima do peso! seu IMC é (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 29.9 && imc < 34.9) {
-        _infoText = "Obesidade grau I! seu IMC é (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está com obesidade grau I! seu IMC é (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 34.9 && imc < 39.9) {
-        _infoText = "Obesidade grau II! seu IMC é (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está com obesidade grau II! seu IMC é (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 40) {
-        _infoText = "Obesidade grau III (${imc.toStringAsPrecision(3)})";
+        _infoText =
+            "Você está com obesidade grau III! seu IMC é (${imc.toStringAsPrecision(3)})";
       }
     });
   }
@@ -125,19 +130,15 @@ class _HomeState extends State<Home> {
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 50),
         child: Form(
           key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Icon(
-                Icons.person_outline,
-                size: 120,
-                color: Colors.indigo,
-              ),
+              Icon(Icons.person_outline, size: 80, color: Colors.indigo),
               TextFormField(
-                keyboardType: TextInputType.number,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
                 decoration: InputDecoration(
                   labelText: "Peso (KG)",
                   labelStyle: TextStyle(color: Colors.indigo),
